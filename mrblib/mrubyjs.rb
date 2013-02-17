9132fb49bc60dcf90a1f125680025aea14b08cca
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2013-02-17 17:06:39 -0500

    Add method_missing tricks, remove useless debug lines.

--- mrblib/mrubyjs.rb
+++ mrblib/mrubyjs.rb
@@ -17,22 +17,24 @@ module MrubyJs
       raise ArgumentError, "#{name} does not exist!" unless func
       func
     end
+
+    # For methods that do not conflict with Ruby, we can directly
+    # call them
+    def method_missing(key, *args)
+      call(key.to_s, *args)
+    end
   end
 
   class JsFunction
     attr_reader :parent_object
 
     def initialize(handle, parent_object)
-      puts "Creating js function! handle: #{handle}, parent: #{parent_object.inspect}"
-
       super(handle)
 
       @parent_object = parent_object
     end
 
     def invoke(*args)
-      puts "Invoke: #{parent_object.inspect}"
-
       invoke_internal(0, *args)
     end
 
@@ -42,6 +44,10 @@ module MrubyJs
 
     def invoke_with_this(this_value, *args)
       invoke_internal(2, this_value, *args)
+    end
+
+    def method_missing(key, *args)
+      invoke(key.to_s, *args)
     end
   end
 end

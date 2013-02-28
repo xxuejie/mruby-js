85ebacc8290a4dcf2bc703003e840ba357c82855
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2013-02-27 21:18:57 -0500

    Fix the case that wrong number of arguments are passed to Ruby side from JS

--- mrblib/mrubyjs.rb
+++ mrblib/mrubyjs.rb
@@ -122,8 +122,23 @@ class Hash
 end
 
 class Symbol
-  def to_proc
+  # Create a proc based on the method specified by the symbol,
+  # we need +num_args+ to indicate the number of arguments passing
+  # from JS side, since it is not allowed to pass less or more
+  # arguments than specified(JS has no this limitation).
+  # Here's two solutions to this problem:
+  # 1. Add an argument indicating the number of arguments
+  # 2. Fix mruby internally to by-pass this restriction
+  # One of webruby's goal is to bring the same Ruby as you run
+  # elsewhere, like in iOS. So we will keep the original Ruby
+  # as much as possible. As a result of this, we will use solution
+  # 1 here by default. At a later time, we may add code to support
+  # solution 2, but the default option will stays 1.
+  def to_proc(num_args = -1)
     sym = self
-    Proc.new { |*args| send(sym, *args) }
+    Proc.new do |*args|
+      args = args[0, num_args] if (num_args != -1 && args.length > num_args)
+      send(sym, *args)
+    end
   end
 end
